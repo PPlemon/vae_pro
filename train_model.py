49373974eeb25f03535f640852dc11f9b6f0f069
PPlemon <53927446+PPlemon@users.noreply.github.com>
2020-10-13 09:03:47 +0800

    解决bug，重新训练

--- train_model.py
+++ train_model.py
@@ -30,41 +30,45 @@ latent_dim = 196
 epochs = 1000
 
 def main():
-    l = [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
-    for i in l:
-        filename = 'data/per_all_base64_' + str(i) + '(120)(2).h5'
-        h5f = h5py.File(filename, 'r')
-        data_train = h5f['smiles_train'][:]
-        data_val = h5f['smiles_val'][:]
-        model = MoleculeVAE()
-        length = len(data_train[0])
-        modelname = 'vae_model_base64_' + str(i) + '(120)(3).h5'
-        print(modelname)
+    # l = [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+    # for i in l:
+    #     filename = 'data/per_all_base64_' + str(i) + '(120)(2).h5'
+    filename = 'data/per_all_base64_250000.h5'
+    h5f = h5py.File(filename, 'r')
+    data_train = h5f['smiles_train'][:]
+    data_val = h5f['smiles_val'][:]
+    model = MoleculeVAE()
+    length = len(data_train[0])
+    # modelname = 'vae_model_base64_' + str(i) + '(120)(3).h5'
+    modelname = 'data/vae_model_base64_250000.h5'
+    print(modelname)
 
-        if os.path.isfile(modelname):
-            model.load(base64_charset_120, length, modelname, latent_rep_size=latent_dim)
-        else:
-            model.create(base64_charset_120, max_length=length, latent_rep_size=latent_dim)
-        check_pointer = ModelCheckpoint(filepath=modelname, verbose=1, save_best_only=True)
+    if os.path.isfile(modelname):
+        model.load(base64_charset_120, length, modelname, latent_rep_size=latent_dim)
+    else:
+        model.create(base64_charset_120, max_length=length, latent_rep_size=latent_dim)
+    check_pointer = ModelCheckpoint(filepath=modelname, verbose=1, save_best_only=True)
 
-        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.0001)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.0001)
 
-        early_stopping = EarlyStopping(monitor='val_loss', patience=20, verbose=2)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=20, verbose=2)
 
-        TensorBoardname = "TensorBoard/vae_model_base64_" + str(i) + '_120_3' 
+    # TensorBoardname = "TensorBoard/vae_model_base64_" + str(i) + '_120_3'
 
-        tbCallBack = TensorBoard(log_dir=TensorBoardname)
+    TensorBoardname = 'TensorBoard/vae_model_base64_250000'
 
-        print(data_train[0])
-        history = model.autoencoder.fit(
-            data_train,
-            data_train,
-            shuffle=True,
-            epochs=epochs,
-            batch_size=batch_size,
-            callbacks=[check_pointer, reduce_lr, early_stopping, tbCallBack],
-            validation_data=(data_val, data_val)
-        )
+    tbCallBack = TensorBoard(log_dir=TensorBoardname)
+
+    print(data_train[0])
+    history = model.autoencoder.fit(
+        data_train,
+        data_train,
+        shuffle=True,
+        epochs=epochs,
+        batch_size=batch_size,
+        callbacks=[check_pointer, reduce_lr, early_stopping, tbCallBack],
+        validation_data=(data_val, data_val)
+    )
 
 if __name__ == '__main__':
     main()

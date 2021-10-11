568fd009f1a58ae69498cb20176edf23ba718c9f
zhihuiqin <root@localhost.localdomain>
2021-10-11 19:46:15 +0800

    2021.10.11

--- train_w2v.py
+++ train_w2v.py
@@ -3,38 +3,41 @@ import h5py
 import jieba
 import pandas as pd
 import pickle
+import numpy as np
 from keras.models import Model,load_model
-#data = pd.read_hdf('/data/tp/data/zinc-1.h5', 'table')
+#data = pd.read_hdf('data/zinc-1.h5', 'table')
 #smiles = data['smiles']
-#logp = data['logp']
-#qed = data['qed']
-#sas = data['sas']
-#sentences = []
-#with open('data.txt', 'w') as f:
+# logp = data['logp']
+# qed = data['qed']
+# sas = data['sas']
+# sentences = []
+
+# with open('data.txt', 'w') as f:
 #    for s in smiles:
-#        s = s.ljust(120)
-#        split_words = [x  for x in s]
+#        # s = s.ljust(120)
+#        split_words = [x for x in s]
 #        result = ' '.join(split_words)
 #        f.writelines("{}\n".format(result))
-
-
-#with open('data.txt', 'r') as f1:
+#
+#
+# with open('data.txt', 'r') as f1:
 #    for line in f1.readlines():
 #        c = line
 #        print(c, len(c), type(c))
 #        break
 
 
-#result = word2vec.LineSentence('data.txt')
-#sentences = []
-#for smiles in result:
-#    smiles = smiles + [' ']*(120-len(smiles))
-#    sentences.append(smiles)
-#model = word2vec.Word2Vec(sentences, vector_size = 120, min_count=0, window=50)
-#model.save('w2v_120.model')
+result = word2vec.LineSentence('data/word2vec/data.txt')
+sentences = []
+for smiles in result:
+   smiles = smiles + [' ']
+   sentences.append(smiles)
+print(sentences[0])
+model = word2vec.Word2Vec(sentences, vector_size=35, min_count=0, window=2, negative=1)
+model.save('model/word2vec/w2v_35_w2_n1.model')
 
-#model = word2vec.Word2Vec.load('w2v_120.model')
-model = load_model('./word2vec.h5')
+# model = word2vec.Word2Vec.load('w2v_120.model')
+# model = load_model('./word2vec.h5')
 filename = '/data/tp/data/per_all_250000.h5'
 h5f = h5py.File(filename, 'r')
 charset = h5f['charset'][:]
@@ -45,9 +48,7 @@ for s in charset:
     print(s)
     w2v_vector[s] = model.wv[s]
 print(w2v_vector['C'])
-#output_smiles = open('w2v_vector_120.pkl', 'wb')
-#pickle.dump(w2v_vector, output_smiles)
-#output_smiles.close()
-
-
-#def cut_word():
+print(np.linalg.norm(w2v_vector['C']))
+output_smiles = open('data/w2v_vector_35_w2_n1.pkl', 'wb')
+pickle.dump(w2v_vector, output_smiles)
+output_smiles.close()

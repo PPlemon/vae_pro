02663a3121b773fdfe5b7e10ccb68354e5ee901e
zhihuiqin <root@localhost.localdomain>
2021-10-28 17:13:15 +0800

    generate bottom mol latent

--- optimization_path_bottom.py
+++ optimization_path_bottom.py
@@ -5,7 +5,9 @@ random.seed(RANDOM_SEED)
 np.random.seed(RANDOM_SEED)
 import tensorflow as tf
 tf.set_random_seed(RANDOM_SEED)
-from molecules.predicted_vae_model import VAE_prop
+#from molecules.predicted_vae_model import VAE_prop
+from molecules.predicted_vae_model_w2v import VAE_prop
+#from molecules.predicted_vae_model_glove import VAE_prop
 import os
 import h5py
 import pickle
@@ -13,30 +15,24 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
-
-# 验证
-h5f = h5py.File('/data/tp/data/per_all_250000.h5', 'r')
-data_train = h5f['smiles_train'][:50000]
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+# optimization path
+#h5f = h5py.File('/data/tp/data/per_all_250000.h5', 'r')
+h5f = h5py.File('/data/tp/data/per_all_w2v_35_w2_n1_250000.h5', 'r')
+data_train = h5f['smiles_train'][:]
 # data_val = h5f['smiles_val'][:]
 # data_test = h5f['smiles_test'][:]
-qed_train = h5f['qed_train'][:50000]
-sas_train = h5f['sas_train'][:50000]
+qed_train = h5f['qed_train'][:]
+sas_train = h5f['sas_train'][:]
 # logp_test = h5f['logp_test'][:]
 target_train = np.array(qed_train)*5 - np.array(sas_train)
 print(max(target_train))
 
-optimization_result = open('data/optimization_result_from_bottom_joint_model(5_qed-sas).pkl', 'rb')
+optimization_result = open('data/optimization_result_from_bottom_w2v(5qed-sas)(std=1).pkl', 'rb')
 optimization_result = pickle.load(optimization_result)
 
-data = open('data/bottom_mol(5_qed-sas).pkl', 'rb')
-data = pickle.load(data)
-temp = []
-for i in optimization_result:
-    temp.append(i[-1])
-ind = temp.index(max(temp))
-path0 = data[optimization_result[0][0]][2]
-path1 = optimization_result[0][1]
-print(data[optimization_result[0][0]][1])
+path0 = optimization_result[6][0]
+path1 = optimization_result[6][1]
 
 length = len(data_train[0])
 charset = len(data_train[0][0])
@@ -44,30 +40,33 @@ charset = len(data_train[0][0])
 h5f.close()
 model = VAE_prop()
 # modelname = 'model/predictor_vae_model_w2v_30_new_250000_12260707(2).h5'
-modelname = '/data/tp/data/model/predictor_vae_model_250000_12260707(5_qed-sas).h5'
+#modelname = '/data/tp/data/model/predictor_vae_model_250000_0(5qed-sas)(std=1).h5'
+modelname = '/data/tp/data/model/predictor_vae_model_w2v_35_w2_n1_250000_0(5qed-sas)(std=1).h5'
 if os.path.isfile(modelname):
     model.load(charset, length, modelname, latent_rep_size=196)
 else:
     raise ValueError("Model file %s doesn't exist" % modelname)
 x_latent = model.encoder.predict(data_train)
+
 print(model.predictor.predict(np.array(path0).reshape(1, 196)), model.predictor.predict(np.array(path1).reshape(1, 196)))
+
 x_latent = x_latent.tolist()
-x_latent.append(path0[0])
-x_latent.append(path1[0])
-print(x_latent[0])
-print(path0)
+x_latent.append(path0)
+x_latent.append(path1)
+
+
 def normalization(data):
     _range = np.max(data) - np.min(data)
     return (data - np.min(data)) / _range
 
-pca = PCA(n_components=2)
+pca = PCA(n_components=50)
 x_latent_proj = pca.fit_transform(x_latent)
 
 del x_latent
 
 x_latent_proj = normalization(x_latent_proj)
-x = x_latent_proj[:, 0]
-y = x_latent_proj[:, 1]
+x = x_latent_proj[:, 3]
+y = x_latent_proj[:, 4]
 
 
 fig = plt.figure(figsize=(5.5, 4.5))
@@ -93,9 +92,10 @@ sns.distplot(x, ax=ax1, hist=False, vertical=True, kde_kws={"shade": True, "colo
 ax2 = fig.add_subplot(spec[0, 1:])
 ax2.get_xaxis().set_visible(False)
 ax2.get_yaxis().set_visible(False)
-a = ax2.scatter(x[:-2], y[:-2], c=target_train, cmap='YlGnBu', marker='.', s=1)
+a = ax2.scatter(x[:-2], y[:-2], c=target_train, cmap='YlGnBu_r', marker='.', s=1)
 ax2.scatter(x[-2], y[-2], marker='*', s=50, color='b')
 ax2.scatter(x[-1], y[-1], marker='*', s=50, color='r')
 ax2.plot([x[-2], x[-1]], [y[-2], y[-1]], color='r')
 fig.colorbar(a, ax=ax2)
-plt.show()
+plt.savefig(fname="picture_1/optimization_path_w2v(5qed-sas)(std=1).png",figsize=[5.5,4.5],bbox_inches='tight')
+#plt.show()
